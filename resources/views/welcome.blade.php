1d1ead185021359c7b390994d99ffd776001c09f
mogataro <negishikouhei1013@gmail.com>
2019-03-11 02:11:12 +0900

    Fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -28,7 +28,8 @@
             }
             .header {
                 background: aliceblue;
-                text-align: left
+                text-align: left;
+                height: 30px;
             }
 
             .content {
@@ -38,7 +39,8 @@
             .footer {
                 background: aliceblue;
                 display: flex;
-                justify-content: center
+                justify-content: center;
+                height: 30px;
             }
 
             .title {
@@ -48,7 +50,7 @@
             a {
                 color: #636b6f;
                 padding: 0 25px;
-                font-size: 13px;
+                font-size: 20px;
                 font-weight: 600;
                 letter-spacing: .1rem;
                 text-decoration: none;

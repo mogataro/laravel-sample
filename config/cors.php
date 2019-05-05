697e1c364160ca01c1ed174d507dd38fae55d036
mogataro <negishikouhei1013@gmail.com>
2019-05-05 22:07:07 +0900

    Fix api

--- config/cors.php
+++ config/cors.php
@@ -13,7 +13,7 @@
 if (env('APP_ENV') == 'local') {
     return [
         'supportsCredentials' => true,
-        'allowedOrigins' => ['http://localhost:*'],
+        'allowedOrigins' => ['https://mogataro-nuxt-sample-app.herokuapp.com:*'],
         'allowedOriginsPatterns' => [],
         'allowedHeaders' => ['*'],
         'allowedMethods' => ['*'],
